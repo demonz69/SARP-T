d33ea02b3548437f1ea943465094db0404fde27f
Tiraj Basnet <92373913+demonz69@users.noreply.github.com>
2026-01-02 22:07:48 +0545

    Adding MQTT

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class VehicleLocation(models.Model):
+    vehicle_id = models.CharField(max_length=50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.vehicle_id
